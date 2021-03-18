4130beeb516efc2af3f2c5bc2382fa73210af642
Anton Katliar <Anton_Katliar@epam.com>
2021-03-18 03:08:36 +0300

    feat: enable cors

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 3000;
 const axios = require('axios');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
